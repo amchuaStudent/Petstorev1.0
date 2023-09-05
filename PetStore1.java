6804198cb98f657d22d25b949a111d50c76948de
amchuaStudent <141467481+amchuaStudent@users.noreply.github.com>
2023-09-05 23:43:12 +0800

    PetStore1.java

--- PetStore1.java
+++ PetStore1.java
@@ -30,23 +30,23 @@ public class PetStore1 {
         String myBreedStr = myBreed.nextLine();  // Read user input
         
         if (myBreedStr.contentEquals("Labrador")) {
-        	System.out.println("Item ID   Product ID          Description                List Price");    
-            System.out.println("EST-22     K9-RT-02    Adult Male Labrador Retriever       $135.50");
-            System.out.println("EST-23     K9-RT-02    Adult Female Labrador Retriever     $145.49");
+        	System.out.println("Item ID   Product ID           Description                List Price");    
+            System.out.println("EST-22     K9-RT-02        Adult Male Labrador Retriever       $135.50");
+            System.out.println("EST-23     K9-RT-02        Adult Female Labrador Retriever     $145.49");
         }  
         else if (myBreedStr.contentEquals("Manx")) {
-        	System.out.println("Item ID   Product ID        Description      List Price");    
-            System.out.println("EST-14     FL-DSH-01       Tailless Manx       $58.50");
-            System.out.println("EST-15     FL-DSH-01       With tail Manx      $23.50");
+        	System.out.println("Item ID   Product ID        Description        List Price");    
+            System.out.println("EST-14     FL-DSH-01          Tailless Manx          $58.50");
+            System.out.println("EST-15     FL-DSH-01          With tail Manx         $23.50");
         }
         else if (myBreedStr.contentEquals("Koi")) {
         	System.out.println("Item ID   Product ID       Description    List Price");    
-            System.out.println("EST-4     FI-FW-01         Spotted Koi      $18.50");
-            System.out.println("EST-5     FI-FW-01         Spotless Koi     $18.50");
+            System.out.println("EST-4     FI-FW-01             Spotted Koi      $18.50");
+            System.out.println("EST-5     FI-FW-01             Spotless Koi     $18.50");
         }
         else if (myBreedStr.contentEquals("Amazon Parrot")) {
-        	System.out.println("Item ID   Product ID           Description          List Price");    
-            System.out.println("EST-22     AV-CB-01      Adul Male Amazon Parrot      $193.50");
+        	System.out.println("Item ID   Product ID           Description           List Price");    
+            System.out.println("EST-22     AV-CB-01          Adul Male Amazon Parrot      $193.50");
         } 
     }
 }
